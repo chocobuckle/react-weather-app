707d26ed4ef8187bf937b3593b9e5a537371e107
Conor Doyle <doyle7@hotmail.com>
2017-09-15 02:45:44 +0100

    Parse URL query string to get city name when component mounts

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
+import { string, shape } from 'prop-types';
 import axios from 'axios';
 import Loading from '../Loading/Loading';
-// import { string } from 'prop-types';
+
 
 class Forecast extends Component {
+  static propTypes = {
+    location: shape({
+      search: string
+    }).isRequired
+  }
+
   state = {
     loading: true
   }
 
   componentDidMount() {
-    const currentWeatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.cityName}&type=accurate&APPID=eaf616267bbf2cc9d4b24b9f0d52bbb4`;
-    // const fiveDayForecastURL = `http://api.openweathermap.org/data/2.5/forecast?q=${this.state.cityName}&APPID=eaf616267bbf2cc9d4b24b9f0d52bbb4`;
-    axios.get(currentWeatherURL)
-      .then((currentWeather) => {
-        console.log(currentWeather.data);
+    const { city } = queryString.parse(this.props.location.search);
+    // const currentWeatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&type=accurate&APPID=eaf616267bbf2cc9d4b24b9f0d52bbb4`;
+    const sevenDayForecastURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=eaf616267bbf2cc9d4b24b9f0d52bbb4&cnt=7`;
+    axios.get(sevenDayForecastURL)
+      .then((response) => {
+        console.log(response.data);
         this.setState(() => {
           return {
             loading: false

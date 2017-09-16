34809045148ba38b3882fac6404c436d53dc8bda
Conor Doyle <doyle7@hotmail.com>
2017-09-16 23:51:39 +0100

    Scaffold out basic UI for 'Forecast' component

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import queryString from 'query-string';
 import { string, shape } from 'prop-types';
 import axios from 'axios';
+import './Forecast.css';
 import Loading from '../Loading/Loading';
+import formatDate from '../../utils/formatDate';
 
 
 class Forecast extends Component {
@@ -18,29 +20,57 @@ class Forecast extends Component {
 
   componentDidMount() {
     const { city } = queryString.parse(this.props.location.search);
-    // const currentWeatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&type=accurate&APPID=eaf616267bbf2cc9d4b24b9f0d52bbb4`;
-    const sevenDayForecastURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=eaf616267bbf2cc9d4b24b9f0d52bbb4&cnt=7`;
+    // const currentWeatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&type=accurate&APPID=b714ec74bbab5650795063cb0fdf5fbe`;
+    const sevenDayForecastURL = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&type=accurate&APPID=b714ec74bbab5650795063cb0fdf5fbe&cnt=7`;
     axios.get(sevenDayForecastURL)
       .then((response) => {
-        console.log(response.data);
+        const { city, list: forcastArr } = response.data;
         this.setState(() => {
           return {
-            loading: false
+            loading: false,
+            city: city.name,
+            forcastArr
           };
         });
       });
   }
 
   render() {
-    const { loading } = this.state;
-
-    if (loading === true) {
+    // const date = new Date(response.data.list[0].dt * 1000);
+    if (this.state.loading === true) {
       return <Loading />;
     }
 
+    const { city, forcastArr } = this.state;
+    const iconNames = forcastArr.map(forcast => forcast.weather[0].icon);
+    const formattedDates = forcastArr.map(forcast => formatDate(forcast.dt));
+
     return (
       <div>
-        Forecast Component
+        <h1 className='forcast__h1'>{city}</h1>
+        {
+          iconNames.map((iconName, i) => {
+            return (
+              <img
+                className='forcast__icon'
+                src={`${process.env.PUBLIC_URL}weather-icons/${iconName}.svg`}
+                alt='weather icon'
+                key={i} // eslint-disable-line react/no-array-index-key
+              />
+            );
+          })
+        }
+        {
+          formattedDates.map(([day, month, date]) => {
+            return (
+              <h2
+                className='forcast__date'
+                key={day}>
+                {day}, {month} {date}
+              </h2>
+            );
+          })
+        }
       </div>
     );
   }
---
+++ src/utils/formatDate.js
@@ -0,0 +1,52 @@
+// Formats the date so that its contents are unabbreviated and properly suffixed, i.e. 'Mon' will
+// become 'Monday', 'Aug' will become 'August', '04' will become '4th', etc.
+function formatDate(dateInMilliseconds) {
+  const unixTimeStamp = new Date(dateInMilliseconds * 1000);
+  const str = unixTimeStamp.toString();
+  const splitString = str.split(' ');
+  let [ day, month, date ] = splitString;
+
+  switch (day) {
+    case 'Mon':
+      day = 'Monday';
+      break;
+    case 'Tue':
+      day = 'Tuesday';
+      break;
+    case 'Wed':
+      day = 'Wednesday';
+      break;
+    case 'Thu':
+      day = 'Thursday';
+      break;
+    case 'Fri':
+      day = 'Friday';
+      break;
+    case 'Sat':
+      day = 'Saturday';
+      break;
+    case 'Sun':
+      day = 'Sunday';
+      break;
+    default:
+      throw new Error('Day of the week abbreviation not recognised.');
+  }
+
+  date = parseInt(date, 10);
+
+  if (date === 1 || date === 21 || date === 31) {
+    date += 'st';
+  } else if (date === 2 || date === 22) {
+    date += 'nd';
+  } else if (date === 3 || date === 23) {
+    date += 'rd';
+  } else if ((date >= 4 && date <= 20) || (date >= 24 && date <= 30)) {
+    date += 'th';
+  } else {
+    throw new Error('Date is not valid.');
+  }
+
+  return [day, month, date];
+} // End of formatDate().
+
+export default formatDate;
